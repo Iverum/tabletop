517aaae2ec3c3d989a1a97e7150fb0e29d81bf60
Blake Hair <blake.hair@sigstr.com>
2019-02-13 12:43:48 -0500

    :construction: Adding the stubs of a navigation structure to the app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { Provider } from 'mobx-react';
-import React from 'react';
+import { Provider } from "mobx-react";
+import React from "react";
 
-import Characters from './components/Characters';
-import CharacterStore from './stores/CharacterStore';
+import Characters from "./components/Characters";
+import CharacterStore from "./stores/CharacterStore";
 
 const stores = {
   characterStore: new CharacterStore()
@@ -12,7 +12,27 @@ class App extends React.Component {
   render() {
     return (
       <Provider {...stores}>
-        <Characters />
+        <>
+          <nav
+            aria-label="main navigation"
+            className="navbar is-fixed-top"
+            role="navigation"
+          >
+            <div className="navbar-brand">
+              <a className="navbar-item" href="#">
+                <span className="icon">
+                  <i className="fas fa-dice-d20" />
+                </span>
+                <span>Tabletop</span>
+              </a>
+            </div>
+            <div className="navbar-menu">
+              <a class="navbar-item is-active">Characters</a>
+              <a class="navbar-item">Moves</a>
+            </div>
+          </nav>
+          <Characters />
+        </>
       </Provider>
     );
   }
