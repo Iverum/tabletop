7fa2743fdfd4f11d3425e5602314f541204a3a97
Blake Hair <blake.hair@gmail.com>
2019-02-13 20:49:51 -0500

    :lipstick: More styling for cards

--- src/components/Moves.js
+++ src/components/Moves.js
@@ -1,30 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import MOVES from '../constants/moves';
 
-const Card = ({ content, name }) => {
-  const [open, setOpen] = useState(false);
-
-  return (
-    <div className="column is-one-quarter">
-      <div className="card">
-        <header className="card-header">
-          <h1 className="card-header-title">{name}</h1>
-          <div className="card-header-icon" onClick={() => setOpen(!open)}>
-            <span class="icon">
-              <i aria-hidden="true" class="fas fa-angle-down" />
-            </span>
-          </div>
-        </header>
-        {open && (
-          <section className="card-content">
-            <div className="content">{content}</div>
-          </section>
-        )}
-      </div>
+const Card = ({ content, name }) => (
+  <section style={{ padding: '1rem', minWidth: '33%', maxWidth: '33%' }}>
+    <div
+      className="card"
+      style={{ display: 'flex', flexGrow: 1, flexDirection: 'column' }}
+    >
+      <header className="card-header">
+        <h1 className="card-header-title">{name}</h1>
+      </header>
+      <section className="card-content">
+        <div className="content">{content}</div>
+      </section>
     </div>
-  );
-};
+  </section>
+);
 
 const Moves = () => (
   <section className="section">
@@ -34,7 +26,14 @@ const Moves = () => (
       </h1>
     </div>
 
-    <div className="columns is-multiline is-centered is-mobile">
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        flexGrow: 1,
+        flexWrap: 'wrap'
+      }}
+    >
       {MOVES.map(move => (
         <Card {...move} />
       ))}
